25c70efc2732e5427cf278d0e18fa736bb94e540
Rafael Silva <73002086+rafael-ds@users.noreply.github.com>
2021-10-23 09:56:28 -0300

    Desenvolvendo o laoyt entrada

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('base/', views.base, name='base')
+    path('entrada/', views.entrada, name='entrada'),
 ]
--- core/views.py
+++ core/views.py
@@ -5,6 +5,6 @@ def index(request):
     return render(request, 'index.html')
 
 
-def base(request):
-    return render(request, 'base.html')
+def entrada(request):
+    return render(request, 'entrada.html')
 
